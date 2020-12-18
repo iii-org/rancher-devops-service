e008f69d986d2dc551532a05b562e47cf58fd690
ziyi-bear <nuuCSIE404>
2020-12-18 08:04:55 +0000

    須轉移到環境才可繼續編寫(需內網測試)

---
+++ rancher-answer-list-example.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## rancher-answer
+# 
+# 讀取chart內的question.yaml資料
+
+# In[1]:
+
+
+## 引入函示庫
+import yaml
+import sys
+import os
+
+
+# In[ ]:
+
+
+os.getenv('GIT_COPY_TOKEN', '')
+
