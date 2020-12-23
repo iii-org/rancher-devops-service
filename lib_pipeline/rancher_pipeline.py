655d0d012f85c31ed038e9b0d3a58d2fb6bd35f6
ziyi-bear <nuuCSIE404>
2020-12-23 09:14:00 +0000

    web_json_測試模擬正常

--- lib_pipeline/rancher_pipeline.py
+++ lib_pipeline/rancher_pipeline.py
@@ -1,4 +1,11 @@
 import yaml
+import os
+
+PIPELINE_TEMPLATE_FILE = './stander-rancher-pipeline.yaml'
+## 部屬環境Config模擬
+DEVOPS_REGISTRY = os.getenv('DEVOPS_REGISTRY', "XX.XX.XX.XX:XXXX")
+REGISTRY_INSECURE = os.getenv('REGISTRY_INSECURE', "false")
+GIT_HOST = os.getenv('GIT_HOST', "10.XX.XX.XX")
 
 def env_rancher_pipeline_generate(PIPELINE_TEMPLATE_FILE, DEVOPS_REGISTRY, REGISTRY_INSECURE, GIT_HOST):
     # 讀取範例檔案 
@@ -10,4 +17,17 @@ def env_rancher_pipeline_generate(PIPELINE_TEMPLATE_FILE, DEVOPS_REGISTRY, REGIS
     temp_pipeline_stages[0]['steps'][0]['env']['PLUGIN_INSECURE'] = REGISTRY_INSECURE
     # 修改第三個階段 
     temp_pipeline_stages[2]['steps'][0]['applyAppConfig']['answers']['git.host'] = GIT_HOST
+    return pipeline_template
+
+def web_service_pipeline(checkmarxEnabled, checkmarxVersion, newmanEnabled, webType):
+    global PIPELINE_TEMPLATE_FILE, DEVOPS_REGISTRY, REGISTRY_INSECURE, GIT_HOST
+    pipeline_template = env_rancher_pipeline_generate(PIPELINE_TEMPLATE_FILE, DEVOPS_REGISTRY, REGISTRY_INSECURE, GIT_HOST)
+    temp_pipeline_stages = pipeline_template['stages']
+    # 把最後面的階段移除就會變成結果
+    temp_pipeline_stages.pop(2)
+    # 將使用者參數帶入
+    app_web_answers = temp_pipeline_stages[1]['steps'][0]['applyAppConfig']['answers']
+    app_web_answers['checkmarx.enabled'] = checkmarxEnabled
+    app_web_answers['newman.enabled'] = newmanEnabled
+    app_web_answers['web.type'] = webType
     return pipeline_template
