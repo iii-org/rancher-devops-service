4050f8a593782efa7aef79131d67932765fcdcfd
ziyi-bear <nuuCSIE404>
2020-12-25 07:08:48 +0000

    統合整理函示庫測試

--- lib_pipeline/rancher_pipeline.py
+++ lib_pipeline/rancher_pipeline.py
@@ -31,3 +31,18 @@ def web_service_pipeline(checkmarxEnabled, checkmarxVersion, newmanEnabled, webT
     app_web_answers['newman.enabled'] = newmanEnabled
     app_web_answers['web.type'] = webType
     return pipeline_template
+
+def db_service_pipeline(db_name, db_type, db_username, db_password):
+    global PIPELINE_TEMPLATE_FILE, DEVOPS_REGISTRY, REGISTRY_INSECURE, GIT_HOST
+    pipeline_template = env_rancher_pipeline_generate(PIPELINE_TEMPLATE_FILE, DEVOPS_REGISTRY, REGISTRY_INSECURE, GIT_HOST)
+    temp_pipeline_stages = pipeline_template['stages']
+    # 把最前面的兩個階段移除
+    temp_pipeline_stages.pop(0)
+    temp_pipeline_stages.pop(0)
+    # 將使用者參數帶入
+    app_web_answers = temp_pipeline_stages[0]['steps'][0]['applyAppConfig']['answers']
+    app_web_answers['db.name'] = db_name
+    app_web_answers['db.type'] = db_type
+    app_web_answers['db.username'] = db_username
+    app_web_answers['db.password'] = db_password
+    return pipeline_template
